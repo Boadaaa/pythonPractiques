b048573eed9c2e8bed7e1c7de96cad26a1e83819
Boadaa <mboada892@boscdelacoma.cat>
2024-02-12 17:29:05 +0100

    entra sense credencials

--- Projecte/powershell.ps1
+++ Projecte/powershell.ps1
@@ -1,8 +1,9 @@
-Connect-VIServer -Server 172.24.69.102
+$User = "root"
+$Password = ConvertTo-SecureString "Patata123*" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($User, $Password)
+Connect-VIServer -Server 172.24.69.102 -Credential $Credential
 
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-
-Get-VMHost
 
 Get-VM | Select-Object Name, PowerState, NumCpu, MemoryMB, Version, Guest
 
