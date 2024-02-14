669a7074824812f16ce3f633c0991c1b3e51a3da
Boadaa <mboada892@boscdelacoma.cat>
2024-02-14 19:46:37 +0100

    correccio errors

--- Projecte/datastore.ps1
+++ Projecte/datastore.ps1
@@ -6,13 +6,15 @@ Connect-VIServer -Server 172.24.69.102 -User root -Password Patata123*
 
 # Nombre de la máquina virtual que deseas crear
 $VMName = "AlpineMaquina"
-$DatastorePath = /vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-standard-3.19.1-aarch64.iso
+$DatastorePath = "/vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-standard-3.19.1-aarch64.iso"
 $DatastoreName = "DATASTORE_GRAN"
 $VLAN = "LAN_24"
 
 # Crea la máquina virtual
-New-VM -Name $VMName -Datastore $DatastoreName -DiskGB 10 -MemoryGB 1 -NetworkName $VLAN -CD -ISOPath $DatastorePath
-GuestId "other3xLinux64Guest" -NumCpu 1 -NetworkName $VLAN -DiskStorageFormat Thin
+New-VM -Name $VMName -Datastore $DatastoreName -DiskGB 10 -MemoryGB 1 -NetworkName $VLAN -CD -HostPath $DatastorePath -GuestId "other3xLinux64Guest" -NumCpu 1 -DiskStorageFormat Thin
 
-# Montar arxiu ISO
-Set-CDDrive -VM $VMName -IsoPath $DatastorePath -StartConnected -Confirm:$false
+# Montar archivo ISO
+Set-CDDrive -VM $VMName -IsoPath $DatastorePath -StartConnected:$true -Confirm:$false
+
+# Desconecta del servidor ESXi
+Disconnect-VIServer -Server 172.24.69.102 -Confirm:$false
