3a50ec41f0be6c8234c3ed8414ff1a3116727860
Boadaa <mboada892@boscdelacoma.cat>
2024-02-14 21:02:04 +0100

    correccio maquina virtual sense iso

--- Projecte/datastore.ps1
+++ Projecte/datastore.ps1
@@ -6,15 +6,12 @@ Connect-VIServer -Server 172.24.69.102 -User root -Password Patata123*
 
 # Nombre de la máquina virtual que deseas crear
 $VMName = "AlpineMaquina"
-$DatastorePath = "/vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/debian-11.8.0-amd64-netinst.iso"
+$DatastorePath = "/vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-standard-3.19.1-aarch64.iso"
 $DatastoreName = "DATASTORE_GRAN"
 $VLAN = "LAN24"
 
 # Crea la máquina virtual y muestra las propiedades seleccionadas
-$VM = New-VM -Name $VMName -Datastore (Get-Datastore -Name $DatastoreName) -MemoryGB 1 -CD -GuestId "other3xLinux64Guest" -NumCpu 1 -DiskStorageFormat Thin | Select-Object -Property PowerState, Guest, NumCpu, CoresPerSocket, MemoryMB, VMHost, Name
-
-# Verificar la ruta del archivo ISO antes de montarlo
-Write-Host "Ruta del archivo ISO: $DatastorePath"
+$VM = New-VM -Name $VMName -Datastore (Get-Datastore -Name $DatastoreName) -MemoryGB 1 -CD -GuestId "other3xLinuxGuest" -NumCpu 1 -DiskStorageFormat Thin | Select-Object -Property PowerState, Guest, NumCpu, CoresPerSocket, MemoryMB, VMHost, Name
 
 # Montar archivo ISO si la ruta es válida
 if (Test-Path $DatastorePath) {
