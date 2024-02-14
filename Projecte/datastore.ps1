68e66407b419fb4f2d3f8133abff4f487439706a
Boadaa <mboada892@boscdelacoma.cat>
2024-02-14 18:44:11 +0100

    creacio maquina virtual

--- Projecte/datastore.ps1
+++ Projecte/datastore.ps1
@@ -8,4 +8,7 @@ Connect-VIServer -Server 172.24.69.102 -User root -Password Patata123*
 $VMName = "AlpineMaquina"
 $DatastorePath = /vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-standard-3.19.1-aarch64.iso
 $DatastoreName = "DATASTORE_GRAN"
+$VLAN = "LAN_24"
 
+# Crea la máquina virtual
+New-VM -Name $VMName -Datastore $DatastoreName -DiskGB 10 -MemoryGB 1 -NetworkName $VLAN -CD -ISOPath $DatastorePath
