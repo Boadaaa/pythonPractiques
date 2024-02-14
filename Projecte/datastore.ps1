3f9da58272cd9c5ebb981179dccb100488a18d17
Boadaa <mboada892@boscdelacoma.cat>
2024-02-14 20:49:43 +0100

    No em posa la iso a la MV

--- Projecte/datastore.ps1
+++ Projecte/datastore.ps1
@@ -10,13 +10,17 @@ $DatastorePath = "/vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-
 $DatastoreName = "DATASTORE_GRAN"
 $VLAN = "LAN24"
 
-# Crea la máquina virtual
-$Datastore = Get-Datastore -Name $DatastoreName
-New-VM -Name $VMName -Datastore $Datastore -MemoryGB 1 -CD -GuestId "other3xLinux64Guest" -NumCpu 1 -DiskStorageFormat Thin | Select-Object -Property PowerState, Guest, NumCpu, CoresPerSocket, MemoryMB, VMHost, Name
+# Crea la máquina virtual y muestra las propiedades seleccionadas
+$VM = New-VM -Name $VMName -Datastore (Get-Datastore -Name $DatastoreName) -MemoryGB 1 -CD -GuestId "other3xLinux64Guest" -NumCpu 1 -DiskStorageFormat Thin | Select-Object -Property PowerState, Guest, NumCpu, CoresPerSocket, MemoryMB, VMHost, Name
 
-# Montar archivo ISO
-$VM = Get-VM -Name $VMName
-Set-CDDrive -CD (Get-CDDrive -VM $VM) -IsoPath $DatastorePath -StartConnected:$true -Confirm:$false
+# Verificar la ruta del archivo ISO antes de montarlo
+Write-Host "Ruta del archivo ISO: $DatastorePath"
+
+# Montar archivo ISO si la ruta es válida
+if (Test-Path $DatastorePath) {
+    Set-CDDrive -CD (Get-CDDrive -VM $VM) -IsoPath $DatastorePath -StartConnected:$true -Confirm:$false
+}
 
 # Desconecta del servidor ESXi
 Disconnect-VIServer -Server 172.24.69.102 -Confirm:$false
+
