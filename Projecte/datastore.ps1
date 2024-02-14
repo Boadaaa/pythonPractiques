4f5c104a4d7e3c589e27a789c3784d5175d4a4dc
Boadaa <mboada892@boscdelacoma.cat>
2024-02-14 18:27:59 +0100

    posar nom a la maquina

--- Projecte/datastore.ps1
+++ Projecte/datastore.ps1
@@ -5,14 +5,5 @@ Import-Module VMware.PowerCLI
 Connect-VIServer -Server 172.24.69.102 -User root -Password Patata123*
 
 # Nombre de la máquina virtual que deseas crear
-$VMName = "MiVM"
-
-# Nombre del datastore donde deseas almacenar la máquina virtual
-$DatastoreName = "LOCAL"
-
-# Ruta completa al datastore
-$DatastorePath = "/vmfs/volumes/$DatastoreName/"
-
-# Crea una nueva máquina virtual
-New-VM -Name $VMName -Datastore $DatastoreName -VMFilePath $DatastorePath -MemoryGB 2 -DiskGB 20
+$VMName = "AlpineMaquina"
 
