983c72e2efc75e62224022a43a6e29bf876d6c0e
Boadaa <mboada892@boscdelacoma.cat>
2024-02-21 19:34:25 +0100

    canviar guestID de 3x a 5x i de 32 bits a 64 bits

--- Projecte/datastore.ps1
+++ Projecte/datastore.ps1
@@ -6,15 +6,15 @@ Connect-VIServer -Server 172.24.69.102 -User root -Password Patata123*
 
 # Nombre de la máquina virtual que deseas crear
 $VMName = "AlpineMaquina"
-$DatastorePath = "/vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-standard-3.19.1-aarch64.iso"
+$DatastorePath = "/vmfs/volumes/659eda0e-7a5a0587-c1a7-9c7bef2a7d69/ISOS/alpine-standard-3.19.1-x86_64.iso"
 $DatastoreName = "DATASTORE_GRAN"
 $VLAN = "LAN24"
 
 # Nombre de la nueva máquina virtual
 $ReplicaVMName = "AlpineMaquina2"
 
-# Crea la máquina virtual y muestra las propiedades seleccionadas
-$VM = New-VM -Name $VMName -Datastore (Get-Datastore -Name $DatastoreName) -MemoryGB 1 -CD -GuestId "other3xLinuxGuest" -NumCpu 1 -DiskStorageFormat Thin | Select-Object -Property PowerState, Guest, NumCpu, CoresPerSocket, MemoryMB, VMHost, Name
+#Crea la máquina virtual y muestra las propiedades seleccionadas
+$VM = New-VM -Name $VMName -Datastore (Get-Datastore -Name $DatastoreName) -MemoryGB 1 -CD -GuestId "other5xLinux64Guest" -NumCpu 1 -DiskStorageFormat Thin | Select-Object -Property PowerState, Guest, NumCpu, CoresPerSocket, MemoryMB, VMHost, Name
 
 # Montar archivo ISO si la ruta es válida
 if (Test-Path $DatastorePath) {
