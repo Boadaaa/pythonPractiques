52ae3eb216e4388df6aba157cd8b6d29bf49053f
Boadaa <mboada892@boscdelacoma.cat>
2024-02-26 16:44:43 +0100

    ultilizar una plantilla

--- Projecte/vsphere.ps1
+++ Projecte/vsphere.ps1
@@ -4,3 +4,4 @@ Connect-VIServer -Server 172.24.69.12 -User administrator@vsphere.local -Passwor
 # Clonar la máquina virtual
 $sourceVM = Get-VM -Name "Alpine"
 $destinationTemplate = "Alpune"
+New-VM -VM $sourceVM -Name $destinationTemplate -ResourcePool (Get-ResourcePool) -Location (Get-Folder -Name "Templates")
