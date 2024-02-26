7c9170e5faf7f3299d622dca637779734da1b771
Boadaa <mboada892@boscdelacoma.cat>
2024-02-26 17:23:43 +0100

    correccio errors

--- Projecte/vsphere.ps1
+++ Projecte/vsphere.ps1
@@ -1,14 +1,18 @@
-#Entrar al Vsphere
+# Conectarse al servidor vSphere
 Connect-VIServer -Server 172.24.69.12 -User administrator@vsphere.local -Password Patata123*
 
-# Clonar la máquina virtual
+# Obtener la máquina virtual que deseas clonar
 $sourceVM = Get-VM -Name "Alpine"
+
+# Clonar la máquina virtual y convertirla en una plantilla
 $destinationTemplate = "Alpune"
-New-VM -VM $sourceVM -Name $destinationTemplate -ResourcePool (Get-ResourcePool) -Location (Get-Folder -Name "Templates")
+New-VM -VM $sourceVM -Name $destinationTemplate -Location (Get-Folder -Name "Templates")
+
+# Obtener la nueva máquina virtual clonada
+$templateVM = Get-VM -Name $destinationTemplate
 
 # Convertir la máquina virtual clonada en una plantilla
-$templateVM = Get-VM -Name $destinationTemplate
-Set-VM -VM $templateVM -Template $true
+Set-VM -VM $templateVM -ToTemplate -Confirm:$false
 
 # Desconectarse del servidor vSphere
 Disconnect-VIServer -Server 172.24.69.12 -Confirm:$false
