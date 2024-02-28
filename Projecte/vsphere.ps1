d0ddd0c53e848f029beceb87745938eb8f0bdc5e
Boadaa <mboada892@boscdelacoma.cat>
2024-02-28 18:46:46 +0100

    engegar maquina

--- Projecte/vsphere.ps1
+++ Projecte/vsphere.ps1
@@ -14,6 +14,11 @@ $esx = Get-VMHost -Name $esxName
 # Crea la nueva máquina virtual a partir de la plantilla
 $vm = New-VM -Template $template -Name 'AlpineMain' -VMHost $esx -Datastore $ds -DiskStorageFormat Thin
 
+# Configura la CPU y la memoria de la nueva máquina virtual
+$vm | Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false
+
+# Inicia la nueva máquina virtual
+$vm | Start-VM -Confirm:$false
 
 # Desconectarse del servidor vSphere
 Disconnect-VIServer -Server 172.24.69.12 -Confirm:$false
