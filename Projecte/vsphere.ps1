848cdd346b148145cecce0dda91d5e2349b6d2c4
Boadaa <mboada892@boscdelacoma.cat>
2024-02-28 18:38:16 +0100

    nom del datastore

--- Projecte/vsphere.ps1
+++ Projecte/vsphere.ps1
@@ -4,6 +4,7 @@ Connect-VIServer -Server 172.24.69.12 -User administrator@vsphere.local -Passwor
 # Define los detalles de la plantilla y del host
 $templateName = 'AlpineClonar'  
 $esxName = '172.24.69.12'  
+$dsName = 'LOCAL'  
 
 
 # Desconectarse del servidor vSphere
