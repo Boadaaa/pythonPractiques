7270ec88289ccb7ce0d317e46a323c165635a9f0
Boadaa <mboada892@boscdelacoma.cat>
2024-02-28 18:37:22 +0100

    nom del template

--- Projecte/vsphere.ps1
+++ Projecte/vsphere.ps1
@@ -1,7 +1,8 @@
 # Conectarse al servidor vSphere
 Connect-VIServer -Server 172.24.69.12 -User administrator@vsphere.local -Password Patata123*
 
-
+# Define los detalles de la plantilla y del host
+$templateName = 'AlpineClonar'  
 
 # Desconectarse del servidor vSphere
 Disconnect-VIServer -Server 172.24.69.12 -Confirm:$false
