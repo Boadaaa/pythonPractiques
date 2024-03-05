d276c946fee913156242d8fb9b8dc46fd54078ca
Boadaa <mboada892@boscdelacoma.cat>
2024-03-05 19:31:40 +0100

    camps especifics

--- Projecte/vsphere.ps1
+++ Projecte/vsphere.ps1
@@ -2,7 +2,7 @@
 Connect-VIServer -Server 172.24.69.12 -User administrator@vsphere.local -Password Patata123*
 
 # Define los detalles de la plantilla y del host
-$templateName = 'AlpineClonar'  
+$templateName = "AlpineClonar"  
 $newVMName = "AlpineMain"
 $folderName = "Templates"
 
@@ -14,14 +14,16 @@ $template = Get-Template -Name $templateName -Location (Get-Folder -Name $folder
 $vmHost = Get-VMHost | Select-Object -First 1
 
 # Crear la nueva máquina virtual
-$vm = New-VM -Template $template -Name 
-$newVMName -VMHost $vmHost -Location (Get-Folder -Name $folderName)
+$vm = New-VM -Name $newVMName -VMHost $vmHost -Location (Get-Folder -Name $folderName) -Template $template
 
 # Configurar la nueva máquina virtual
 $vm | Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false
 
 # Iniciar la nueva máquina virtual
 $vm | Start-VM -Confirm:$false
+
+# Mostrar los campos específicos
+$vm | Select-Object Guest, NumCpu, MemoryMB, VMHost, GuestId, Name
 
 # Desconectarse del servidor vSphere
 Disconnect-VIServer -Server 172.24.69.12 -Confirm:$false
