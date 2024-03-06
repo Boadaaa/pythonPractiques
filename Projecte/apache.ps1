bb2ff8999c6cb0bcf09ecdd72ba8afa5365b80da
Boadaa <mboada892@boscdelacoma.cat>
2024-03-06 18:47:17 +0100

    he cambiat la manera de entrar perque l'altre manera no funcionava

--- Projecte/apache.ps1
+++ Projecte/apache.ps1
@@ -1,37 +1,30 @@
 # Conectarse al servidor vSphere
 Connect-VIServer -Server 172.24.69.12 -User administrator@vsphere.local -Password Patata123*
 
-# Obtener la máquina virtual
-$vm = Get-VM -Name AlpineMain
+# Define la dirección IP de la máquina virtual
+$ip = "172.24.69.222"
 
-# Establecer la dirección IP y el puerto SSH de la máquina virtual
-$sshAddress = "172.24.69.222"
-
-# Establecer el nombre de usuario y la contraseña para SSH
-$username = "troll"
-$password = "Patata123*"
-
-# Crear un objeto PSCredential para la autenticación SSH
-$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
-
-# Conectar por SSH a la máquina virtual
-$session = New-SSHSession -ComputerName $sshAddress -Credential $credential -Port $sshPort
-
-# Ejecutar un comando para verificar el estado del servicio de Apache
-$result = Invoke-SSHCommand -SessionId $session.SessionId -Command "service apache2 status"
-
-# Mostrar el resultado
-if ($result.Output -match "active") {
-    Write-Host "El servicio Apache está funcionando en la máquina virtual."
-} else {
-    Write-Host "El servicio Apache no está funcionando en la máquina virtual."
+# Comprueba si el servicio Apache está funcionando en la máquina virtual
+function CheckApacheStatus {
+    try {
+        $response = Invoke-WebRequest -Uri "http://$ip" -TimeoutSec 5 -ErrorAction Stop
+        if ($response.StatusCode -eq 200) {
+            return $true
+        } else {
+            return $false
+        }
+    } catch {
+        return $false
+    }
 }
 
-# Cerrar la sesión SSH
-Remove-SSHSession -SessionId $session.SessionId
+# Verifica si el servicio Apache está funcionando en la máquina virtual antes de continuar
+if (CheckApacheStatus) {
+    Write-Host "El servei Apache si funca en la maquina virtual."
+} else {
+    Write-Host "El servei Apache no funca en la maquina virtual."
+    exit 1 
+}
 
 # Desconectarse del servidor vSphere
 Disconnect-VIServer -Server 172.24.69.12 -Confirm:$false
-
-
